c14723b527e073ef6f465ba3c1063205ee278d20
arshsiddiqui <arshsiddiqui@gmail.com>
2022-07-22 00:49:58 -0400

    changes to files as requested

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,15 @@ app.get("/", (req, res) => {
 //add new item to json file
 app.post("/add/item", addItem)
 
+const basicAuth = require("express-basic-auth");
+var { authenticator, upsertUser, cookieAuth } = require("./authentication");
+const auth = basicAuth({
+    authorizer: authenticator
+});
+const cookieParser = require("cookie-parser");
+app.use(cookieParser("82e4e438a0705fabf61f9854e3b575af"));
+
+
 function addItem (request, response) {
     // Converting Javascript object (Task Item) to a JSON string
     let id = request.body.jsonObject.id
@@ -47,9 +56,10 @@ app.get("/get/items", getItems)
     var data = fs.readFileSync('database.json');
     
     //uncomment to see the data being returned 
-    //console.log(JSON.parse(data));
+    console.log(JSON.parse(data));
 
     response.json(JSON.parse(data));
+    response.send();
     // Note this won't work, why? response.send();
   } 
 
@@ -59,13 +69,42 @@ app.get("/get/searchitem",searchItems)
     //begin here
     var searchField = request.query.taskname;
     //uncomment to see the searchField passed in
-    //console.log(searchField);
+    console.log(searchField);
 
     var json = JSON.parse (fs.readFileSync('database.json'));
     returnData = json.filter(jsondata => jsondata.Task === searchField);
 
     //uncomment to see the todolists found in the backend service// 
-    //console.log(returnData);
+    console.log(returnData);
     response.json(returnData);
+    response.send();
     //Note this won't work, why? response.send();
   }
+
+
+
+
+  app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+  }));
+
+
+
+  app.get("/authenticate", auth, (req, res) => {
+    console.log(`user logging in: ${req.auth.user}`);
+    res.cookie('user', req.auth.user, { signed: true });
+    res.sendStatus(200);
+  });
+
+  app.post("/users", (req, res) => {
+      const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+      const [username, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+      const upsertSucceeded = upsertUser(username, password)
+      res.sendStatus(upsertSucceeded ? 200 : 401);
+  });
+
+  app.get("/logout", (req, res) => {
+      res.clearCookie('user');
+      res.end();
+  });
